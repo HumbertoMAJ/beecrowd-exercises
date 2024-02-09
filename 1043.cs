121ee940f5d0f93e166bd892159a8a8aa68013d6
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-02-09 02:14:01 -0300

    Add files via upload

---
+++ 1043.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string valores = Console.ReadLine();
+
+        string[] separar = valores.Split(' ');
+
+        double a = double.Parse(separar[0]);
+        double b = double.Parse(separar[1]);
+        double c = double.Parse(separar[2]);
+
+
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            double param = ((a + b) + c);
+
+            Console.WriteLine("Perimetro = "+ param.ToString("0.0", CultureInfo.InvariantCulture));
+
+        }
+        else
+        {
+            double area = ((a + b)*c /2);
+           
+            Console.WriteLine("Area = "+ area.ToString("0.0", CultureInfo.InvariantCulture));
+        }
+
+
+
+    }
+
+}
