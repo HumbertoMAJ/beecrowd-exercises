4c9d5c24098ceccddf65c581d718eea1f1d4b64a
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-02-10 00:22:01 -0300

    Add files via upload

---
+++ 1044.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string valor = Console.ReadLine();
+
+        string[] v = valor.Split(' ');
+
+        int a = int.Parse(v[0]);
+        int b = int.Parse(v[1]);
+
+        if (a % b == 0 || b % a == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+
+
+
+    }
+
+}
