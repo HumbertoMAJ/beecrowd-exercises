cf5fc190aa0b8ec55e6cdadecbea8431944c8058
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-03-01 22:04:53 -0300

    Add files via upload

---
+++ 1046.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string entrada = Console.ReadLine();
+
+        string[] valores = entrada.Split(' ');
+
+        int a = int.Parse(valores[0]);
+        int b = int.Parse(valores[1]);
+        int resultado;
+
+        if (a > b)
+        {
+            resultado = (24 - a) + b;
+            Console.WriteLine("O JOGO DUROU "+resultado+" HORA(S)");
+
+        }
+        else if (a == b)
+        {
+            resultado = 24;
+            Console.WriteLine("O JOGO DUROU " + resultado + " HORA(S)");
+
+        }
+        else if (a < b)
+        {
+            resultado = b - a;
+            Console.WriteLine("O JOGO DUROU " + resultado + " HORA(S)");
+        }
+        
+
+        
+
+    }
+
+}
