aee535333319c3f71b4a763ab2666b9f45a4d927
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-03-08 00:06:16 -0300

    Add files via upload

---
+++ 1048.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string entrada = Console.ReadLine();
+
+        double valor = double.Parse(entrada, CultureInfo.InvariantCulture);
+
+  
+        double calculo;
+        double salario;
+
+
+        if(valor <= 400.00)
+        {
+            
+            calculo = valor * 0.15;
+            salario = valor + calculo;
+
+            Console.WriteLine("Novo salario: "+salario.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: "+calculo.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: 15 %");
+
+        }else if (valor >= 400.01 && valor <= 800.00)
+        {
+            calculo = valor * 0.12;
+            salario = valor + calculo;
+
+            Console.WriteLine("Novo salario: " + salario.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + calculo.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: 12 %");
+
+        }else if (valor >= 800.01 && valor <= 1200.00)
+        {
+            calculo = valor * 0.10;
+            salario = valor + calculo;
+
+            Console.WriteLine("Novo salario: " + salario.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + calculo.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: 10 %");
+        
+        }else if (valor >= 1200.01 && valor <= 2000.00)
+        {
+            calculo = valor * 0.07;
+            salario = valor + calculo;
+
+            Console.WriteLine("Novo salario: " + salario.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + calculo.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: 7 %");
+       
+        }else if (valor > 2000.00 )
+        {
+            calculo = valor * 0.04;
+            salario = valor + calculo;
+
+            Console.WriteLine("Novo salario: " + salario.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + calculo.ToString("0.00", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: 4 %");
+        }
+
+    }
+
+}
