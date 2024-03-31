c7d4c04a4465a989f2791d177982ed1e2dc58808
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-03-31 02:04:17 -0300

    Add files via upload

---
+++ 1060.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int contagem = 0;
+
+        for (int i = 1; i < 7; i++)
+        {
+            
+            string valor = Console.ReadLine();
+            double numero = double.Parse(valor);
+            
+            if (numero >=0)
+            {
+                contagem++;
+            }           
+        }
+        Console.WriteLine(contagem+ " valores positivos");
+
+      
+
+
+    }
+
+}
