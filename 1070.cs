a6c08e40728a0f044b54865105931ccce990f466
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-05-09 12:35:46 -0300

    Add files via upload

---
+++ 1070.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string entrada = Console.ReadLine();
+        int convert = int.Parse(entrada);
+        int contador = 0;
+
+        for(int i = 0; i <= 20; i++)
+        {
+            if (convert % 2 != 0)
+            {
+                Console.WriteLine(convert);
+                contador++;
+            }
+            convert++;
+
+            if(contador == 6)
+            {
+                break;
+            }
+        }
+
+    }
+
+}
