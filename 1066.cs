54a46449e64bda439d406843d739bc0eeec514cb
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-05-09 00:39:08 -0300

    Add files via upload

---
+++ 1066.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int pares = 0;
+        int impares = 0;
+        int positivo = 0;
+        int negativo = 0;
+
+        for (int i = 0; i <= 4; i++)
+        {
+            string entrada = Console.ReadLine();
+
+            int convert = int.Parse(entrada);
+
+            int[] valores = new int[5];
+
+            valores[i] = convert;
+
+
+
+            if (valores[i] % 2 == 0)
+            {
+                pares++;
+            }
+            else
+            {
+                impares++;
+            }
+
+            if (valores[i] > 0)
+            {
+                positivo++;
+            }
+            else if(valores[i] < 0)
+            {
+                negativo++;
+            }
+
+        };
+
+        Console.WriteLine(pares + " valor(es) par(es)");
+        Console.WriteLine(impares + " valor(es) impar(es)");
+        Console.WriteLine(positivo + " valor(es) positivo(s)");
+        Console.WriteLine(negativo + " valor(es) negativo(s)");
+
+    }
+
+}
