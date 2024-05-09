6d9cde46258734dbe09cb8ab2af245dc08658c3f
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-05-09 00:28:39 -0300

    Add files via upload

---
+++ 1065.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int numero = 0;
+
+        for (int i =0; i <= 4; i++)
+        {
+            string entrada = Console.ReadLine();
+
+            int convert = int.Parse(entrada);
+
+            int[] valores = new int[5];
+
+            valores[i] = convert;
+
+           
+
+            if (valores[i] % 2 == 0)
+            {
+                numero++;
+            }
+           
+
+        };
+
+        Console.WriteLine(numero + " valores pares");
+
+    }
+
+}
