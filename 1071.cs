534a519f1154a0bda8083f9d9513e4e7bae48cee
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-05-10 15:26:04 -0300

    Add files via upload

---
+++ 1071.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int maior, menor;
+        int soma = 0;
+
+        int X = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
+
+        if( X < Y)
+        {
+            maior = Y;
+            menor = X;
+        }
+        else
+        {
+            maior = X;
+            menor = Y;
+        }
+        
+
+        for(int i = menor+1; i < maior; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma = soma + i;
+            }
+        }
+        Console.WriteLine(soma);
+
+
+    }
+
+}
