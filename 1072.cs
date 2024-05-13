1e8fe4b06c2e2f1d8f53ee4004f51438d094dc68
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-05-12 23:36:38 -0300

    Add files via upload

---
+++ 1072.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int inn = 0;
+        int outt = 0;
+
+        int N = int.Parse(Console.ReadLine());
+
+        if(N < 10000)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+
+                int X = int.Parse(Console.ReadLine());
+
+                if (X >= 10 && X <= 20) 
+                {
+                    inn++;
+                }
+                else
+                {
+                    outt++;
+                }
+
+            }
+
+            Console.WriteLine(inn+" in");
+            Console.WriteLine(outt+" out");
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+}
