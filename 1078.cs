837f4b1a5205b1d27b998fee63a4e7c107ae8221
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-08-11 21:46:26 -0300

    Add files via upload

---
+++ 1078.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int N = int.Parse(Console.ReadLine());
+
+        if (N > 2 && N < 1000) 
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine(i + " x " + N + " = " + i * N);
+
+
+            }
+
+
+
+        }
+
+    }
+
+}
