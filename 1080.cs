b6f9a763785d2e75b2816553a857d3c17e0f5838
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-08-13 00:21:10 -0300

    Add files via upload

---
+++ 1080.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int[] N = new int[100];
+        int maiorvalor = 0;
+
+        for (int i = 0; i < 100; i++) 
+        {
+
+            string input = Console.ReadLine();
+
+            int convert = int.Parse(input);
+
+            N[i]  = convert;
+
+        }
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (N[i] > maiorvalor)
+            {
+                maiorvalor = N[i];
+            }
+
+        }
+
+        Console.WriteLine(maiorvalor);
+        int indice = Array.IndexOf(N, maiorvalor);
+        Console.WriteLine(indice+1);
+
+
+
+    }
+
+}
