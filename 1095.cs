44ae48170ee495566ea877f17025d2e61e853680
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-08-23 22:01:36 -0300

    Add files via upload

---
+++ 1095.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int I = 1;
+        int J = 60;
+
+        while (J >= 0) 
+        { 
+            Console.WriteLine(@"I="+I+" J="+J);
+            I += 3;
+            J -= 5;
+        }
+
+    }
+
+}
