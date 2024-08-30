f8d078612236fd6254365ca7b9b29bb27387f2c7
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-08-30 00:22:46 -0300

    Add files via upload

---
+++ 1113.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        for (int i = 0; i < 50; i++)
+        {
+            string ent = Console.ReadLine();
+
+            string[] s = ent.Split(' ');
+
+            int v1 = int.Parse(s[0]);
+            int v2 = int.Parse(s[1]);
+
+            if (v1 == v2)
+            {
+                break;
+
+            }
+            else if (v1 > v2)
+            {
+                Console.WriteLine("Decrescente");
+            }
+            else
+            {
+                Console.WriteLine("Crescente");
+            }
+
+        }
+
+    }
+}
