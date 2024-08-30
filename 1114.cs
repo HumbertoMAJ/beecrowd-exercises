657805848e31401412194ce5c06e39046d5de7aa
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2024-08-30 00:47:34 -0300

    Add files via upload

---
+++ 1114.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int senha = 2002;
+        int tentativas = 1;
+
+        for (int i = 0; i < tentativas; i++) 
+        {
+            int senha2 = int.Parse(Console.ReadLine());
+
+            if (senha2 != senha) 
+            {
+                Console.WriteLine("Senha Invalida");
+                tentativas++;
+            }
+            else
+            {
+                Console.WriteLine("Acesso Permitido");
+            }
+        }
+
+    }
+
+}
