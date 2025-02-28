8c70561ac8d8af2d1f52eee5c71be92b8d459bef
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2025-02-28 00:19:40 -0300

    Add files via upload

---
+++ 1132.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada = int.Parse(Console.ReadLine());
+            int entrada2 = int.Parse(Console.ReadLine());
+            int calculado = 0;
+
+            
+            if (entrada > entrada2)
+            {
+                int temp = entrada;
+                entrada = entrada2;
+                entrada2 = temp;
+            }
+
+            
+            for (int i = entrada; i <= entrada2; i++)
+            {
+                if (i % 13 != 0)
+                {
+                    calculado += i;
+                }
+            }
+
+            Console.WriteLine(calculado);
+        }
+    }
+}
