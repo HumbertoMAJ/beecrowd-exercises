462c0c9b592fc8896d5630308cc33684e4784ae0
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2025-02-28 00:20:10 -0300

    Add files via upload

---
+++ 1133.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1133
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            int valor2 = int.Parse(Console.ReadLine());
+
+            // Corrigindo a troca de valores
+            if (valor > valor2)
+            {
+                int temp = valor;
+                valor = valor2;
+                valor2 = temp;
+            }
+
+            for (int i = valor + 1; i < valor2; i++)
+            {
+                if (i % 5 == 2 || i % 5 == 3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
