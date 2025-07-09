da3abac5558ffaced30fef21de1e69c9a3f3afd2
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2025-07-08 21:44:57 -0300

    Add files via upload

---
+++ 1142.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+
+namespace _1142
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int numero = 0;
+
+            if (N > 0) 
+            {
+                         
+                for (int i = 0; i < N; i++) 
+                {
+
+                Console.WriteLine(++numero + " "+ ++numero + " " + ++numero  + " PUM");
+                numero++;
+
+                }
+
+
+            }
+
+        }
+    }
+}
