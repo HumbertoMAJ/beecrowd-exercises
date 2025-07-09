2f66855aa8a11f784977bb2d63f157adf9123060
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2025-07-08 22:09:48 -0300

    Add files via upload

---
+++ 1143.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1143
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int numero = 0;
+            int numero2 = 0;
+            int numero3 = 0;
+
+            if (N > 1 && N < 1000)
+            {
+                for (int i = 0; i < N; i++)
+                {
+
+                    ++numero;
+
+                    numero2 = numero * numero;
+
+                    numero3 = numero * numero2;
+
+                    Console.WriteLine(numero + " " + numero2 + " " + numero3);
+
+
+                }
+
+                {
+
+                }
+            }
+        }
+    }
+}
