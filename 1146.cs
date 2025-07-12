9caf3a87dadb450e8b8cb8b35f020caa32115b17
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2025-07-11 21:37:21 -0300

    Add files via upload

---
+++ 1146.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int entrada = int.Parse(Console.ReadLine());
+
+            if (entrada == 0)
+                break;
+
+            List<string> numeros = new List<string>();
+            for (int i = 1; i <= entrada; i++)
+            {
+                numeros.Add(i.ToString());
+            }
+
+            Console.WriteLine(string.Join(" ", numeros));
+        }
+    }
+}
