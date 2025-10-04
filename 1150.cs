387babeea7c3cc178c5843dcf36152a9d29db680
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2025-10-04 14:59:20 -0300

    Add files via upload

---
+++ 1150.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int X = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
+        bool vf = true;
+        int aux = 0;
+        int conta= 0;
+        int N = 0;
+
+        
+            while (vf)
+            {
+                if (X > Y || X == Y)
+                {
+                    Y = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                vf = false;
+            }
+             
+            }
+
+
+        vf= true;
+        while (vf)
+        {
+            aux = aux + X++;
+            N++;
+            conta = X + aux;
+
+            if (conta > Y)
+            {
+                Console.WriteLine(N+1);
+                vf = false;
+            }
+        }
+       
+
+       
+       
+          
+       
+
+
+
+    }
+
+}
