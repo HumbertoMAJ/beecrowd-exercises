929875338c81fc52ac3af66783bd0a5fa803c91e
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2026-01-13 22:49:04 -0300

    Add files via upload

---
+++ 1151.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int entrada = int.Parse(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+        int c;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.Append(a);
+        sb.Append(" " + b);
+
+        for (int i = 0; i < entrada - 2; i++)
+        {
+            c = a + b;
+            sb.Append(" " + c);
+            a = b;
+            b = c;
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
