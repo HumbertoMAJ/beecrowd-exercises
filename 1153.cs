2e484b69c6c29991dc08e9be0f2a7b29e1ca0d1c
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2026-02-02 21:50:20 -0300

    Add files via upload

---
+++ 1153.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int N = int.Parse(Console.ReadLine());
+        int X = 0;
+
+        if (N > 0 && N < 13)
+        {
+            X = N;
+
+            for (int i = 1; i < X; i++)
+            {
+                N = N * (X - i);
+
+            }
+
+            Console.WriteLine(N);
+
+        }
+
+    }
+}
