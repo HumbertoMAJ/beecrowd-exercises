273b9732f3b08f1b5e54833d3499b05a6220d3d3
Humberto Moura <79175703+HumbertoMAJ@users.noreply.github.com>
2026-02-02 22:26:26 -0300

    Add files via upload

---
+++ 1154.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int N = 1;
+        int X = 0;
+        double cont = 0;
+
+        while (N > 0)
+        {
+            N = int.Parse(Console.ReadLine());
+            
+
+
+            if (N > 0)
+            {
+                X++;
+                cont = cont + N;
+            }
+
+            if(N < 0)
+            {
+                double resultado = cont / X;
+                Console.WriteLine(resultado.ToString("0.00", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+
+
+    }
+
+}
